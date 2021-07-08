94869b20dd97da42d0bfd9e9c67cd63f9a6d8802
FrankMS369 <frankms2000@outlook.com>
2021-07-07 23:08:23 -0500

    Le agregé el >:v

--- app/src/main/java/com/example/pruebax100000/MainActivity.java
+++ app/src/main/java/com/example/pruebax100000/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //YAAAAAAA WWWEEYYY POR FAVOOOORRRRRR//
+        //Aver si si >:V//
     }
 }
